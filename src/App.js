0ea9e87f3a6a48e8c700800ed459b442fdbe094a
andriivam <andriivlasiuk2@gmail.com>
2024-06-09 21:48:55 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import fishingSpots from './fishingSpots.json';
 
 function App() {
+
+  console.log(fishingSpots, 'fishing spots')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Fishing Map</h2>
+      <MapContainer className="map" center={[50.83563307812127, 4.355027921482965]} zoom={20} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {fishingSpots.map(loc => (<Marker key={loc.name} position={[loc.lat, loc.long]}>
+          <Popup>
+            {loc.name}
+            <br />
+            {loc.description}
+          </Popup>
+        </Marker>))}
+      </MapContainer>
     </div>
+
   );
 }
 
