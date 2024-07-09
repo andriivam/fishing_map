2241bb114da76754297531c14b57616cb12a39c8
andriivam <andriivlasiuk2@gmail.com>
2024-07-09 09:17:48 +0200

    added new fishing spots

--- src/components/MapC/Map.jsx
+++ src/components/MapC/Map.jsx
@@ -6,6 +6,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 const Map = ({ fishingSpots }) => {
 
 
+
     return (
         <div>
             <MapContainer className="map" center={[50.83563307812127, 4.355027921482965]} zoom={8} scrollWheelZoom={false}>
@@ -13,17 +14,22 @@ const Map = ({ fishingSpots }) => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {fishingSpots.map((loc) => (<Marker key={loc.name} position={[loc.lat, loc.long]}>
-                    <Popup className="leaflet-popup-content-wrapper"
-                        style={{ backgroundColor: loc.color }}
+                {fishingSpots.map((loc) => (
+                    <Marker
+
+                        key={loc.name}
+                        position={[loc.lat, loc.long]}
                     >
-                        <div style={{ backgroundColor: loc.color }}>
-                            {loc.name}
-                            <br />
-                            {loc.description}
-                        </div>
-                    </Popup>
-                </Marker>))}
+                        <Popup
+                            style={{ backgroundColor: loc.color }}
+                        >
+                            <div style={{ backgroundColor: loc.color }}>
+                                {loc.name}
+                                <br />
+                                {loc.description}
+                            </div>
+                        </Popup>
+                    </Marker>))}
             </MapContainer>
         </div>
     )
